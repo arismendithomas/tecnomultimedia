8134832483e7eaeabbb754b113e2644584c5d2d3
arismendithomas <128560392+arismendithomas@users.noreply.github.com>
2023-10-24 18:50:56 -0300

    clase_principal.js

--- clase_principal.js
+++ clase_principal.js
@@ -1,4 +1,3 @@
-
 class clasePrincipal {
 
   constructor() {
@@ -18,9 +17,7 @@ class clasePrincipal {
     this.velocidad = 4;
     this.velocidadbala = 4;
     this.velocidadbalay = 10;
-    this.numbala =0;
     this.direccion = [];
-    //cosas que pueden ir en reset
     for (let i=0; i<6; i++) {
       if (i/2===0) {
         this.direccion[i] = 1;
@@ -32,7 +29,6 @@ class clasePrincipal {
     for (let i=0; i<6; i++) {
       this.alienx[i]=random(0, 600);
     }
-    //cosas que pueden ir en reset
     this.alieny = [100, 148, 195, 242, 290, 338];
     this.alien = new alien();
     this.ancho=99;
@@ -45,14 +41,57 @@ class clasePrincipal {
 
   display() {
     this.mostrar();
+    textSize(20);
+    text(this.orden, 50, 50);
   }
 
   accionbotones() {
-    if (this.orden===0 && this.botones.accionboton(45, 500, 238, 90)) {
-      this.orden=1;
+    if (this.orden===0) { // inicio a instrucciones
+      if (this.botones.accionboton(45, 500, 238, 90)) { // portada a intrucciones
+        this.orden=1;
+      }
     }
-    if (this.orden===1 && this.botones.accionboton(250, 598, 210, 72)) {
-      this.orden=2;
+    if (this.orden===1) { // instrucciones a juego
+      if (this.botones.accionboton(258, 593, 183, 70)) { //instrucciones a juego
+        this.orden=2;
+      }
+    }
+
+    if (this.orden===3) {//perdiste a creditos
+      if (this.botones.accionboton(397, 560, 180, 50)===true) {
+        //this.orden=5;
+        this.creditos();
+        fill(255);
+        rect(400,400,200,200);
+      }
+    }
+    
+    if (this.orden===3) { //perdiste a inicio
+      if (this.botones.accionboton(415, 465, 135, 50)===true) {
+        this.reseteo();
+      }
+    }
+
+
+    if (this.orden===4) {// ganaste a creditos
+      if (this.botones.accionboton(471, 604, 177, 49)===true) {
+        this.orden=5;
+        fill(255);
+        rect(400,400,200,200);
+      }
+    }
+    
+    if (this.orden===4) { //ganaste a inicio
+      if (this.botones.accionboton(49, 602, 136, 51)=== true) {
+        this.reseteo();
+      }
+    }
+
+
+    if (this.orden===5) { //creditos a inicio
+      if (this.botones.accionboton(277, 592, 134, 50)=== true) {
+        this.reseteo();
+      }
     }
   }
 
@@ -70,6 +109,12 @@ class clasePrincipal {
         this.bala.balay = 473;
         this.contador.num++;
       }
+      if (this.pierdevida()) {
+        this.disparo=false;
+        this.bala.balax = this.mama.mx+28;
+        this.bala.balay = 473;
+        this.vida.numcora -=1;
+      }
     }
   }
 
@@ -83,16 +128,9 @@ class clasePrincipal {
 
   pierdevida () {
     if (this.bala.balay <= 0 ) {
-        return true;
-      } else {
-        return false;
-      }
-    }
-  
-  condividas() {
-
-    if (this.pierdevida() === true && this.vida.numcora <= 3) {
-      this.vida.numcora -=1;
+      return true;
+    } else {
+      return false;
     }
   }
 
@@ -105,97 +143,92 @@ class clasePrincipal {
     this.mama.dibujarmujer();
     if (this.mama.mx < -90) {
       this.mama.mx = width;
-      this.bala.balax = width +62;
+      this.bala.balax = width +28;
     }
   }
 
   mostrar() {
+    
     if (this.orden===0) {
-      this.escena.ponerescena(this.numescenas);
+      this.escena.ponerescena(this.numescenas = 0);
     }
     if (this.orden===1) {
       this.escena.ponerescena(this.numescenas=1);
     }
     if (this.orden===2) {
       this.escena.ponerescena(this.numescenas=2);
-
-      if (this.teclaD) {
-        this.mama.mx += this.velocidad;
-      }
-      if (this.teclaD && this.disparo===false) {
-        this.bala.balax += this.velocidadbala;
-      }
-
-      if (this.teclaA) {
-        this.mama.mx -= this.velocidad;
-      }
-      if (this.teclaA && this.disparo===false) {
-        this.bala.balax -= this.velocidadbala;
-      }
-
-      if (this.disparo) {
-        this.bala.balay -= this.velocidadbalay;
-      }
-
       this.balaymujer();
       this.aliens();
       this.contador.dibujarcontador();
       this.vida.dibujarcorazon();
-      this.condividas();
+      this.condimov();
+    }
+    if (this.contador.num === 6) {
+      this.orden =4;
+    }
+    if (this.vida.numcora === 0) {
+      this.orden=3;
+    }
+    if (this.orden===3) { //perder
+      this.escena.ponerescena(this.numescenas=3);
+    }
 
+    if (this.orden===4) { //ganar
+      this.escena.ponerescena(this.numescenas=4);
+    }
 
-      if (this.orden===3) { //perder
-        this.escena.ponerescena(this.numescenas = 3);
+    if (this.orden===5) { //creditos
+      this.escena.ponerescena(this.numescenas= 5);
+    }
+  }
+creditos(){
+rect(400,600, 650, 200);
+}
+  reseteo() {
+    this.orden=0;
+    this.balax=28;
+    this.balay=473;
+    this.mamax=0;
+    this.mamay=460;
+    this.disparo = false;
+    this.alienx = [];
+    this.alieny = [100, 148, 195, 242, 290, 338];
+    this.contador.num=0;
+    this.vida.numcora=3;
+    for (let i=0; i<6; i++) {
+      if (i/2===0) {
+        this.direccion[i] = 1;
+      } else {
+        this.direccion[i] = -1;
       }
-      if (this.orden===4) { //ganar
-        this.escena.ponerescena(this.numescenas = 4);
-      }
-      if (this.orden===5) { //creditos
-        this.escena.ponerescena(this.numescenas = 5);
-      }
+    }
+    this.alienx = [];
+    for (let i=0; i<6; i++) {
+      this.alienx[i]=random(0, 600);
+    }
+    //cosas que pueden ir en reset
+  }
+
+  condimov() {
+    if (this.teclaD) {
+      this.mama.mx += this.velocidad;
+    }
+    if (this.teclaD && this.disparo===false) {
+      this.bala.balax += this.velocidadbala;
+    }
+
+    if (this.teclaA) {
+      this.mama.mx -= this.velocidad;
+    }
+    if (this.teclaA && this.disparo===false) {
+      this.bala.balax -= this.velocidadbala;
+    }
+
+    if (this.disparo) {
+      this.bala.balay -= this.velocidadbalay;
     }
   }
 
-
-  /*
-  condicorazones() {
-   this.corazones.dibujarvidas();
-   
-   if (this.bala.balay===0 && this.coraencurso===0) {
-   this.coraencurso +=1;
-   
-   
-   
-   if (this.orden === 3) {
-   this.coraencurso=0;
-   this.contador.num=0;
-   this.mama.mx = 0;
-   this.bala.balay = 473;
-   this.bala.balax = this.mama.mx+28;
-   this.disparo = false;
-   this.alienx=[];
-   this.alieny=[100, 148, 195, 242, 290, 338];
-   this.orden = 0;
-   }
-   if (this.coraencurso===3) {
-   this.orden=3;
-   }
-   if (this.orden===4) {
-   this.coraencurso=0;
-   this.contador.num=0;
-   this.mama.mx = 0;
-   this.bala.balay = 473;
-   this.bala.balax = this.mama.mx+28;
-   this.disparo = false;
-   this.alienx=[];
-   this.alieny=[100, 148, 195, 242, 290, 338];
-   this.orden = 0;
-   }
-   if (this.contador.num === 6) {
-   this.orden = 4;
-   }
-   }
-   */
 
   accionteclas() {
 
@@ -207,12 +240,9 @@ class clasePrincipal {
     }
     if (key === 'p' || key === 'P') {
       this.disparo = true;
-    } else if  (this.bala.balay<0 && key === 'r' || key === 'R') {
-      this.bala.balay = 473;
-      this.bala.balax = this.mama.mx+28;
-      this.disparo = false;
     }
   }
+  
   noaccionteclas() {
 
     if (key === 'a' || key === 'A') {
